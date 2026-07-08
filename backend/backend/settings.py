6601b23c7f1936849e4c179ee713fe845a340674
Prathibaa <p.prathibaa07>
2026-07-09 00:37:12 +0530

    Allow all hosts in Django settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1zt#=1o)pthx#sbps1g(efvjn+wdx&3rd193yn0ervr@hn6b56
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
